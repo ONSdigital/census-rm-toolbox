78dcf2cb9738308a7e2030dce80f33a710f78851
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-09-11 15:18:41 +0100

    Handle missing stats

--- poison_message_queue_finder.py
+++ poison_message_queue_finder.py
@@ -16,25 +16,42 @@ def main():
     all_queues = response.json()
 
     for queue in all_queues:
-        queue_name = queue['name']
+        try:
+            queue_name = queue['name']
+        except [TypeError, KeyError]:
+            print(f'Unexpected data for queue: {queue}')
+            pass
+
         queue_details = requests.get(
             f'http://{Config.RABBITMQ_HOST}:{Config.RABBITMQ_HTTP_PORT}/api/queues/{v_host}/{queue_name}',
             auth=HTTPBasicAuth(Config.RABBITMQ_USER, Config.RABBITMQ_PASSWORD)).json()
 
+        redeliver_rate = 0
+        publish_rate = 0
+        ack_rate = 0
+
         try:
             redeliver_rate = queue_details['message_stats']['redeliver_details']['rate']
-            publish_rate = queue_details['message_stats']['publish_details']['rate']
-            ack_rate = queue_details['message_stats']['ack_details']['rate']
-
-            json_to_log = {
-                "queue_name": queue_name,
-                "redeliver_rate": redeliver_rate,
-                "publish_rate": publish_rate,
-                "ack_rate": ack_rate
-            }
-            print(json.dumps(json_to_log))
         except KeyError:
             pass
+
+        try:
+            publish_rate = queue_details['message_stats']['publish_details']['rate']
+        except KeyError:
+            pass
+
+        try:
+            ack_rate = queue_details['message_stats']['ack_details']['rate']
+        except KeyError:
+            pass
+
+        json_to_log = {
+            "queue_name": queue_name,
+            "redeliver_rate": redeliver_rate,
+            "publish_rate": publish_rate,
+            "ack_rate": ack_rate
+        }
+        print(json.dumps(json_to_log))
 
 
 if __name__ == "__main__":
