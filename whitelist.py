895883210c9fa150028072d780441378094fa58b
Ryan Grundy <ryan.grundy@ext.ons.gov.uk>
2019-09-25 15:38:44 +0100

    Added args

--- whitelist.py
+++ whitelist.py
@@ -13,31 +13,45 @@ BEFORE RUNNING:
 3. Install the Python client library for Google APIs by running
    `pip install --upgrade google-api-python-client`
 """
+import argparse
 import os
 from pprint import pprint
 
 from googleapiclient import discovery
 from oauth2client.client import GoogleCredentials
 
-credentials = GoogleCredentials.get_application_default()
 
-service = discovery.build('container', 'v1', credentials=credentials)
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Tool to whitelist a cloudshell IP in a projects RM cluster')
+    parser.add_argument('project_id', help='Target project ID', type=str)
+    parser.add_argument('ip_address', help='IP address to whitelist', type=str)
+    return parser.parse_args()
 
 
-request = service.projects().locations().clusters().get(name=f'projects/census-rm-ryangrundy/locations/europe-west2/clusters/rm-k8s-cluster')
-response = request.execute()
+def main():
+    args = parse_arguments()
+    credentials = GoogleCredentials.get_application_default()
 
-current_authorised_networks = response['masterAuthorizedNetworksConfig']
+    service = discovery.build('container', 'v1', credentials=credentials)
 
-new_ip = {'displayName': 'New IP',
-          'cidrBlock': '130.211.59.47/32'}
+    request = service.projects().locations().clusters().get(
+        name=f'projects/{args.project_id}/locations/europe-west2/clusters/rm-k8s-cluster')
+    response = request.execute()
 
-new_authorised_networks = {'update': {'desiredMasterAuthorizedNetworksConfig': current_authorised_networks}}
+    current_authorised_networks = response['masterAuthorizedNetworksConfig']
+    new_ip = {'displayName': f'{os.getenv("USER")}_cloudshell',
+              'cidrBlock': f'{args.ip_address}/32'}
 
-new_authorised_networks['update']['desiredMasterAuthorizedNetworksConfig']['cidrBlocks'].append(new_ip)
+    new_authorised_networks = {'update': {'desiredMasterAuthorizedNetworksConfig': current_authorised_networks}}
 
-update_request = service.projects().locations().clusters().update(name=f'projects/census-rm-ryangrundy/locations'
-                                                                       f'/europe-west2/clusters/rm-k8s-cluster',
-                                                                  body=new_authorised_networks)
+    new_authorised_networks['update']['desiredMasterAuthorizedNetworksConfig']['cidrBlocks'].append(new_ip)
 
-update_request.execute()
+    update_request = service.projects().locations().clusters().update(name=f'projects/{args.project_id}/locations'
+                                                                           f'/europe-west2/clusters/rm-k8s-cluster',
+                                                                      body=new_authorised_networks)
+
+    update_request.execute()
+
+
+if __name__ == '__main__':
+    main()
