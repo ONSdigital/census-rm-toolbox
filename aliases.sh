28d99657c49dbae8a2d48ce68f73ae57cf7a00e2
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-10-10 12:29:32 +0100

    WIP

--- aliases.sh
+++ aliases.sh
@@ -1,6 +1,6 @@
 shopt -s expand_aliases
 alias queuetool='python queue_manipulator.py'
-alias findbadmessages='python poison_message_queue_finder.py -r'
+alias findbadqueues='python poison_message_queue_finder.py -r'
 alias findpubsubmessages='python get_pubsub_messages.py'
 alias pubsubmessagetobucket='python put_message_on_bucket.py'
 alias buckettopubsub='python publish_message_from_bucket.py'
@@ -11,3 +11,12 @@ alias dumpfilestoqueue='python dump_files_to_queue.py'
 alias dumpqueuetofiles='python dump_queue_to_files.py'
 alias listqueues='python poison_message_queue_finder.py'
 alias makemessage='python message_maker.py'
+alias findbadmessages='curl http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessages | jq'
+alias peekmessage='curl http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/peekmessage'
+alias skipmessage='curl http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/skipmessage'
+alias viewskipped='curl http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/skippedmessages | jq'
+alias resetexceptionmanager='curl http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/reset'
+
+badmessagedetails() {
+    curl http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessage/$1 | jq
+}
