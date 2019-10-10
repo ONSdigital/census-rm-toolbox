f2737bf94aa7d488ccf659e5094a14f5687bf980
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-10-10 12:51:28 +0100

    Update aliases and help

--- aliases.sh
+++ aliases.sh
@@ -6,16 +6,16 @@ alias pubsubmessagetobucket='python put_message_on_bucket.py'
 alias buckettopubsub='python publish_message_from_bucket.py'
 alias dosql='dosql.sh'
 alias dosql-i-know-what-i-am-doing='i_know_what_i_am_doing.sh n00dleBla5terBadg3r'
-alias helpme='echo "Commands: findbadmessages, queuetool [-h], vi, curl, dosql [username], dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage"'
+alias helpme='echo "Commands: findbad, baddetails, queuetool [-h], vi, curl, dosql [username], sftp, dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage, peekmessage, skipmessage, viewskipped, resetexceptionmanager"'
 alias dumpfilestoqueue='python dump_files_to_queue.py'
 alias dumpqueuetofiles='python dump_queue_to_files.py'
 alias listqueues='python poison_message_queue_finder.py'
 alias makemessage='python message_maker.py'
-alias findbadmessages='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessages | jq'
+alias findbad='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessages | jq'
 alias viewskipped='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/skippedmessages | jq'
 alias resetexceptionmanager='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/reset'
 
-badmessagedetails() {
+baddetails() {
     curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessage/$1 | jq
 }
 
