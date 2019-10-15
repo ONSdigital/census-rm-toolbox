ffab51aae8e2eca461a1daa74d7167b21f859934
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2019-10-15 13:10:55 +0100

    Add Exception Manager to Toolbox (#16)

--- aliases.sh
+++ aliases.sh
@@ -1,13 +1,32 @@
 shopt -s expand_aliases
 alias queuetool='python queue_manipulator.py'
-alias findbadmessages='python poison_message_queue_finder.py -r'
+alias findbadqueues='python poison_message_queue_finder.py -r'
 alias findpubsubmessages='python get_pubsub_messages.py'
 alias pubsubmessagetobucket='python put_message_on_bucket.py'
 alias buckettopubsub='python publish_message_from_bucket.py'
 alias dosql='dosql.sh'
 alias dosql-i-know-what-i-am-doing='i_know_what_i_am_doing.sh n00dleBla5terBadg3r'
-alias helpme='echo "Commands: findbadmessages, queuetool [-h], vi, curl, dosql [username], dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage"'
+alias helpme='echo "Commands: findbad, baddetails, queuetool [-h], vi, curl, dosql [username], sftp, dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage, peekmessage, skipmessage, viewskipped, resetexceptionmanager"'
 alias dumpfilestoqueue='python dump_files_to_queue.py'
 alias dumpqueuetofiles='python dump_queue_to_files.py'
 alias listqueues='python poison_message_queue_finder.py'
 alias makemessage='python message_maker.py'
+alias findbad='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessages | jq'
+alias viewskipped='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/skippedmessages | jq'
+alias resetexceptionmanager='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/reset'
+
+baddetails() {
+    curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessage/$1 | jq
+}
+
+peekmessage() {
+    echo "----PEEK START----"
+    echo
+    curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/peekmessage/$1
+    echo
+    echo "-----PEEK END-----"
+}
+
+skipmessage() {
+    curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/skipmessage/$1
+}
