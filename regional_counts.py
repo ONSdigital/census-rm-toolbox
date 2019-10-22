735f380a6a00812b445d44e904d6117e97a00c81
liamtoozer <liam.toozer@gmail.com>
2019-10-22 13:22:09 +0100

    Use inline table for conditional output

--- regional_counts.py
+++ regional_counts.py
@@ -4,16 +4,18 @@ from utilities.db_helper import execute_sql_query
 
 if __name__ == "__main__":
     sql_query = """
-SELECT CASE LEFT(region, 1)
+SELECT CASE counts.region
            WHEN 'E' THEN 'England'
            WHEN 'W' THEN 'Wales'
            WHEN 'N' THEN 'Northern Ireland'
-           ELSE region END AS region, COUNT(*)
-FROM casev2.cases
-WHERE receipt_received = true
-AND action_plan_id = '432f0597-0076-4adb-834b-bf249dc06ded'
-GROUP BY region;
-        """
+           ELSE 'Unknown Region: ' || counts.region END,
+       counts.region_count
+FROM (SELECT LEFT(region, 1) AS region, COUNT(*) AS region_count
+      FROM casev2.cases
+      WHERE receipt_received = true
+        AND action_plan_id = '432f0597-0076-4adb-834b-bf249dc06ded'
+      GROUP BY LEFT(region, 1)) AS counts
+    """
 
     db_result = execute_sql_query(sql_query)
 
