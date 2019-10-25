07f767453147ef6625c494eb1dfdcc7299a318dd
LeoKHoward <leo-howard@hotmail.co.uk>
2019-10-25 14:06:20 +0100

    Included messages to be sent

--- questionnaire_linking.py
+++ questionnaire_linking.py
@@ -1,30 +1,31 @@
 import csv
 import argparse
-import os
 import requests
 import qid_checksum_validator
 from config import Config
+from datetime import datetime
+import uuid
+import json
+from utilities.rabbit_context import RabbitContext
 
 
-def validate_sample_file(sample_file_path):
+def validate_qid_link_file(qid_link_file_path):
     try:
-        with open(sample_file_path, encoding="utf-8") as sample_file: #open() opens CSV as text file
-            return load_sample(sample_file)
+        with open(qid_link_file_path, encoding="utf-8") as qid_link_file:
+            return load_sample(qid_link_file)
     except UnicodeDecodeError as err:
         print(f'Invalid file encoding, requires utf-8, error: {err}')
         exit(-1)
 
 
-def load_sample(sample_file):
-    sample_file_reader = csv.DictReader(sample_file, delimiter=',', fieldnames=['case_ref', 'qid']) #DictReader() reads CSV data directly into a dictionary / delimiter specifies character used to separate each field
-    validate_and_submit_questionnaire_links(sample_file_reader)
+def load_sample(qid_link_file):
+    qid_link_file_reader = csv.DictReader(qid_link_file, delimiter=',', fieldnames=['case_ref', 'qid'])
+    validate_and_submit_questionnaire_links(qid_link_file_reader)
 
 
-#argparse makes it easier to write user-friendly command-line interfaces
-#also automatically generates help/usage messages/issues errors when users give the program invalid arguments
 def parse_arguments():
-    parser = argparse.ArgumentParser(description='Load a sample file into response management.')
-    parser.add_argument('sample_file_path', help='path to the sample file', type=str)
+    parser = argparse.ArgumentParser(description='Load a QID Link file into response management.')
+    parser.add_argument('qid_link_file_path', help='path to the QID Link file', type=str)
     return parser.parse_args()
 
 
@@ -32,51 +33,88 @@ args = parse_arguments()
 error_count = 0
 
 
-def validate_and_submit_questionnaire_links(sample_file_reader):
-    for line_number, questionnaire_link in enumerate(sample_file_reader, 1): #remove whitespace
+def validate_and_submit_questionnaire_links(qid_link_file_reader):
+    global error_count
+    for line_number, questionnaire_link in enumerate(qid_link_file_reader, 1):
+        error_count = 0
         for i in questionnaire_link:
             questionnaire_link[i] = questionnaire_link[i].replace(" ", "")
-        if (not validate_case_ref(line_number, questionnaire_link["case_ref"])
-                or not validate_qid(line_number, questionnaire_link["qid"])):
-            continue
-        validate_case_id(line_number, questionnaire_link["case_ref"])
+        validate_case_ref(line_number, questionnaire_link["case_ref"])
+        validate_qid_len_and_type(line_number, questionnaire_link["qid"])
+        validate_qid_third_digit(line_number, questionnaire_link["qid"])
+        validate_check_digits(line_number, questionnaire_link["qid"])
+        case_id = validate_case_id(line_number, questionnaire_link["case_ref"])
+
+        if error_count > 0:
+            print(f'{questionnaire_link} on line {line_number} has FAILED')
+        else:
+            post_message_to_queue(case_id, questionnaire_link["qid"])
+        print("\n")
 
 
 def validate_case_ref(line_number, case_ref):
+    global error_count
     if len(case_ref) != 8 or not case_ref.isdigit():
         print(f'Error: Either wrong length or type located on case_ref {case_ref}, line {line_number}')
-        return False
-    return True
+        error_count += 1
 
 
-def validate_qid(line_number, qid):
+def validate_qid_len_and_type(line_number, qid):
+    global error_count
     if len(qid) != 16 or not qid.isdigit():
         print(f'Error: Either wrong length or type located on qid {qid}, line {line_number}')
-    elif qid[2] != "2":
+        error_count += 1
+
+
+def validate_qid_third_digit(line_number, qid):
+    global error_count
+    if qid[2] != "2":
         print(f'QID number {qid}, line {line_number} tranche identifier must be 2')
-    elif not qid_checksum_validator.validate(qid, int(os.getenv('QID_MODULUS')), int(os.getenv('QID_FACTOR')))[0]:
-        print(f'QID check digits incorrect at QID {qid}, line {line_number}')
-    else:
-        return True
-    return False
+        error_count += 1
+
+
+def validate_check_digits(line_number, qid):
+    global error_count
+    if not qid_checksum_validator.validate(qid, int(Config.QID_MODULUS), int(Config.QID_FACTOR))[0]:
+        print(f'Check digits incorrect for QID {qid}, line {line_number}')
+        error_count += 1
 
 
 def validate_case_id(line_number, case_ref):
+    global error_count
     response = requests.get(f'http://{Config.CASEAPI_HOST}:{Config.CASEAPI_PORT}/cases/ref/{case_ref}')
     if response.status_code == 404:
-        print(f'Error: Case_ref {case_ref}, line {line_number} does not exist')
+        print(f'Error: Case_ref {case_ref} on line {line_number} does not exist')
+        error_count += 1
         return None
     response.raise_for_status()
     return response.json()["id"]
 
 
+def post_message_to_queue(case_id, qid):
+    message = {
+        "event": {
+            "type": "QUESTIONNAIRE_LINKED",
+            "source": "RM",
+            "channel": "RM",
+            "dateTime": datetime.utcnow().isoformat() + 'Z',
+            "transactionId": str(uuid.uuid4())
+        },
+        "payload": {
+            "uac": {
+                "questionnaireId": qid,
+                "caseId": case_id,
+            }
+        }
+    }
+    with RabbitContext() as rabbit:
+        rabbit.publish_message(json.dumps(message), "application/json", None, exchange='events',
+                               routing_key='event.questionnaire.update')
+        print(f'Case Reference {case_id} and QID {qid} have PASSED')
+
+
 def main():
-    validate_sample_file(args.sample_file_path)
-    if error_count == 0:
-        print(f'Sample file is OK.')
-    else:
-        print(f'{error_count} error(s) found in sample file')
-        exit(-1)
+    validate_qid_link_file(args.qid_link_file_path)
 
 
 if __name__ == "__main__":
