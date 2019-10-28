65171e15e0257f1a816e54af203ed8f83cf603ee
LeoKHoward <leo-howard@hotmail.co.uk>
2019-10-28 11:01:10 +0000

    Error message changes

--- questionnaire_linking.py
+++ questionnaire_linking.py
@@ -46,37 +46,37 @@ def validate_and_submit_questionnaire_links(qid_link_file_reader):
         case_id = validate_case_id(line_number, questionnaire_link["case_ref"])
 
         if error_count > 0:
-            print(f'{questionnaire_link} on line {line_number} has FAILED')
+            print(f'Error: {questionnaire_link} on line {line_number} have FAILED')
         else:
-            post_message_to_queue(case_id, questionnaire_link["qid"])
+            post_message_to_queue(case_id, questionnaire_link["qid"], line_number)
         print("\n")
 
 
 def validate_case_ref(line_number, case_ref):
     global error_count
     if len(case_ref) != 8 or not case_ref.isdigit():
-        print(f'Error: Either wrong length or type located on case_ref {case_ref}, line {line_number}')
+        print(f'Error: Either wrong length or type located on Case Reference {case_ref}, line {line_number}')
         error_count += 1
 
 
 def validate_qid_len_and_type(line_number, qid):
     global error_count
     if len(qid) != 16 or not qid.isdigit():
-        print(f'Error: Either wrong length or type located on qid {qid}, line {line_number}')
+        print(f'Error: Either wrong length or type located on QID {qid}, line {line_number}')
         error_count += 1
 
 
 def validate_qid_third_digit(line_number, qid):
     global error_count
     if qid[2] != "2":
-        print(f'QID number {qid}, line {line_number} tranche identifier must be 2')
+        print(f'Error: QID {qid}, line {line_number} tranche identifier must be 2')
         error_count += 1
 
 
 def validate_check_digits(line_number, qid):
     global error_count
     if not qid_checksum_validator.validate(qid, int(Config.QID_MODULUS), int(Config.QID_FACTOR))[0]:
-        print(f'Check digits incorrect for QID {qid}, line {line_number}')
+        print(f'Error: Check Digits incorrect for QID {qid}, line {line_number}')
         error_count += 1
 
 
@@ -84,14 +84,14 @@ def validate_case_id(line_number, case_ref):
     global error_count
     response = requests.get(f'http://{Config.CASEAPI_HOST}:{Config.CASEAPI_PORT}/cases/ref/{case_ref}')
     if response.status_code == 404:
-        print(f'Error: Case_ref {case_ref} on line {line_number} does not exist')
+        print(f'Error: Case Reference {case_ref} on line {line_number} does not exist')
         error_count += 1
         return None
     response.raise_for_status()
     return response.json()["id"]
 
 
-def post_message_to_queue(case_id, qid):
+def post_message_to_queue(case_id, qid, line_number):
     message = {
         "event": {
             "type": "QUESTIONNAIRE_LINKED",
@@ -110,7 +110,7 @@ def post_message_to_queue(case_id, qid):
     with RabbitContext() as rabbit:
         rabbit.publish_message(json.dumps(message), "application/json", None, exchange='events',
                                routing_key='event.questionnaire.update')
-        print(f'Case Reference {case_id} and QID {qid} have PASSED')
+        print(f'Case ID {case_id} and QID {qid} on line {line_number} have PASSED')
 
 
 def main():
