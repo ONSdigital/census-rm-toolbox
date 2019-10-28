31e300947a2c4e2d80b02a6c3ea54e421319eadb
ryangrundy7 <35301276+ryangrundy7@users.noreply.github.com>
2019-10-28 15:22:35 +0000

    Create fulfilment count files (#22)
    * Created scripts to run fulfilment counts

--- aliases.sh
+++ aliases.sh
@@ -6,7 +6,7 @@ alias pubsubmessagetobucket='python put_message_on_bucket.py'
 alias buckettopubsub='python publish_message_from_bucket.py'
 alias dosql='dosql.sh'
 alias dosql-i-know-what-i-am-doing='i_know_what_i_am_doing.sh n00dleBla5terBadg3r'
-alias helpme='echo "Commands: helpme, msgwizard, findbad, baddetails, queuetool [-h], vi, curl, dosql [username], sftp, dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage, peekmessage, skipmessage, viewskipped, resetexceptionmanager, qidcheck [qid]"'
+alias helpme='echo "Commands: helpme, msgwizard, findbad, baddetails, queuetool [-h], vi, curl, dosql [username], sftp, dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage, peekmessage, skipmessage, viewskipped, resetexceptionmanager, qidcheck [qid], fulfilment, weekendfulfilment"'
 alias dumpfilestoqueue='python dump_files_to_queue.py'
 alias adventure='python adventure.py'
 alias dumpqueuetofiles='python dump_queue_to_files.py'
@@ -17,6 +17,8 @@ alias viewskipped='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/
 alias resetexceptionmanager='curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/reset'
 alias msgwizard='python bad_message_wizard.py'
 alias qidcheck='python qid_checksum_validator.py --modulus $QID_MODULUS --factor $QID_FACTOR'
+alias fulfilment='python fulfilment_count.py'
+alias weekendfulfilment='python weekend_fulfilment_count.py'
 
 baddetails() {
     curl -s http://$EXCEPTIONMANAGER_HOST:$EXCEPTIONMANAGER_PORT/badmessage/$1 | jq
---
+++ fulfilment_count.py
@@ -0,0 +1,43 @@
+import argparse
+import csv
+from getpass import getpass
+
+import psycopg2
+
+from config import Config
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Tool to get fulfilment counts ')
+    parser.add_argument('fulfilment_date_from', help='From date e.g. 2019-10-18T16:00:00+01:00', type=str)
+    parser.add_argument('fulfilment_date_to', help='To date e.g. 2019-10-22T16:00:00+01:00', type=str)
+    parser.add_argument('username', help='Username to connect to database', type=str)
+    return parser.parse_args()
+
+
+def fulfilment_query(fulfilment_date_from, fulfilment_date_to, username, password):
+    sql_query = """SELECT event_payload ->> 'fulfilmentCode' AS fulfilment_code,
+     count(*)
+     FROM casev2.event
+      WHERE rm_event_processed BETWEEN %s AND %s
+      AND event_type = 'FULFILMENT_REQUESTED'
+      AND event_payload ->> 'fulfilmentCode' LIKE 'P_%%'
+      GROUP BY event_payload ->> 'fulfilmentCode';
+      """
+    conn = psycopg2.connect(f"dbname='{Config.DB_NAME}' user={username} host='{Config.DB_HOST}' "
+                            f"password={password} port='{Config.DB_PORT}'{Config.DB_USESSL}")
+    cur = conn.cursor()
+    cur.execute(sql_query, (fulfilment_date_from, fulfilment_date_to,))
+    db_result = cur.fetchall()
+
+    with open(f'fulfilment-{fulfilment_date_to[:10]}.csv', 'w') as out:
+        csv_out = csv.writer(out)
+        csv_out.writerow(['treatment_code', 'count'])
+        for row in db_result:
+            csv_out.writerow(row)
+
+
+if __name__ == "__main__":
+    args = parse_arguments()
+    password = getpass()
+    fulfilment_query(args.fulfilment_date_from, args.fulfilment_date_to, args.username, password)
