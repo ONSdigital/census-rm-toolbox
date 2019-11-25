717d0c409c8fcb0c17d3a9875774ff7d7fe40672
Nick Grant <nick.grant@ext.ons.gov.uk>
2019-11-25 14:51:16 +0000

    Change if exists logic for drop schema

--- groundzero/destroy_schemas.sql
+++ groundzero/destroy_schemas.sql
@@ -1,28 +1,5 @@
 begin transaction;
-    IF EXISTS(
-        SELECT schema_name
-          FROM information_schema.schemata
-          WHERE schema_name = 'casev2'
-      )
-    THEN
-        drop schema casev2 cascade;
-    END IF;
-
-    IF EXISTS(
-        SELECT schema_name
-          FROM information_schema.schemata
-          WHERE schema_name = 'actionv2'
-      )
-    THEN
-        drop schema actionv2 cascade;
-    END IF;
-
-    IF EXISTS(
-        SELECT schema_name
-          FROM information_schema.schemata
-          WHERE schema_name = 'uacqid'
-      )
-    THEN
-        drop schema uacqid cascade;
-    END IF;
+drop schema IF EXISTS casev2 cascade;
+drop schema IF EXISTS actionv2 cascade;
+drop schema IF EXISTS uacqid cascade;
 commit transaction;
