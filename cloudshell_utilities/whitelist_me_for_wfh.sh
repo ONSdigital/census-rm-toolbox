bdad6c1abdf5742a7dfb0e461b7a27cac10646c3
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-03-25 08:36:48 +0000

    Restore current project at end of WFH scripts

--- cloudshell_utilities/whitelist_me_for_wfh.sh
+++ cloudshell_utilities/whitelist_me_for_wfh.sh
@@ -1,5 +1,7 @@
 pushd "${0%/*}" || exit 1
 
+CURRENT_PROJECT=$(gcloud config get-value project 2> /dev/null)
+
 gcloud auth application-default login
 
 WFH_IP=$(dig +short myip.opendns.com @resolver1.opendns.com)
@@ -20,5 +22,7 @@ pipenv run python whitelist_service_ip.py $WFH_IP ops || exit 1
 pipenv run python whitelist_service_ip.py $WFH_IP rabbitmqmanagement || exit 1
 pipenv run python whitelist_service_ip.py $WFH_IP case-api-test || exit 1
 pipenv run python whitelist_db_ip.py $WFH_IP "$USER" census-rm-blacklodge
+
+gcloud config set project $CURRENT_PROJECT
 
 popd || exit
