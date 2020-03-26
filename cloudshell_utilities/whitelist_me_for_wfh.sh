6f8323385b066501cf6a7c4b446fb524a0a2de3a
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-03-26 12:00:56 +0000

    Update whitelist_me_for_wfh.sh

--- cloudshell_utilities/whitelist_me_for_wfh.sh
+++ cloudshell_utilities/whitelist_me_for_wfh.sh
@@ -1,6 +1,7 @@
 pushd "${0%/*}" || exit 1
 
 CURRENT_PROJECT=$(gcloud config get-value project 2> /dev/null)
+echo Current project is $CURRENT_PROJECT
 
 gcloud auth application-default login
 
@@ -25,5 +26,6 @@ pipenv run python whitelist_db_ip.py $WFH_IP "$USER" census-rm-blacklodge
 
 gcloud config set project $CURRENT_PROJECT
 gcloud container clusters get-credentials rm-k8s-cluster --region europe-west2 --project $CURRENT_PROJECT
+echo Restored current project to $(gcloud config get-value project 2> /dev/null)
 
 popd || exit
