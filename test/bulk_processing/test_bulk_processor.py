72b8d02bc3d432ff271784fefeac6b1fe06393be
Ryan Grundy <ryan.grundy@ons.gov.uk>
2020-07-16 13:30:26 +0100

    Fix bulk processor tests

--- test/bulk_processing/test_bulk_processor.py
+++ test/bulk_processing/test_bulk_processor.py
@@ -1,6 +1,6 @@
 import json
 from functools import partial
-from unittest.mock import Mock, call, ANY
+from unittest.mock import Mock, call
 
 from bulk_processing.bulk_processor import BulkProcessor
 from bulk_processing.processor_interface import Processor
@@ -9,10 +9,8 @@ from bulk_processing.validators import Invalid
 
 def test_run_validation_successful(patching_storage, patching_rabbit, tmp_path):
     # Given
-    mock_processor = Mock(spec=Processor)
-    mock_processor.schema = {'header_1': [], 'header_2': []}
     test_message = {"test_message": "blah"}
-    mock_processor.build_event_messages.return_value = [test_message]
+    mock_processor = setup_mock_processor({'header_1': [], 'header_2': []}, test_message)
     bulk_processor = BulkProcessor(mock_processor)
     bulk_processor.working_dir = tmp_path
     mock_blob = Mock()
@@ -30,9 +28,10 @@ def test_run_validation_successful(patching_storage, patching_rabbit, tmp_path):
     mock_upload_to_bucket = patching_storage.Client.return_value.bucket.return_value.blob.return_value \
         .upload_from_filename
     mock_upload_to_bucket.assert_called_once_with(str(tmp_path.joinpath('processed_mock_blob_name')))
-    patching_rabbit.publish_message.assert_called_once_with(message=json.dumps(test_message),
-                                                            content_type='application/json', headers=None,
-                                                            exchange=ANY, routing_key=ANY)
+    patching_rabbit.return_value.__enter__.return_value.publish_message.assert_called_once_with(
+        message=json.dumps(test_message),
+        content_type='application/json', headers=None,
+        exchange=mock_processor.exchange, routing_key=mock_processor.routing_key)
     assert_no_left_over_files(tmp_path)
 
 
@@ -60,19 +59,21 @@ def test_run_header_validation_fails(patching_storage, patching_rabbit, tmp_path
     assert len(mock_upload_calls) == 2, 'Upload to bucket should be called twice'
     assert call(str(tmp_path.joinpath('failed_mock_blob_name'))) in mock_upload_calls
     assert call(str(tmp_path.joinpath('failure_reasons_mock_blob_name'))) in mock_upload_calls
+    patching_rabbit.return_value.__enter__.return_value.publish_message.assert_not_called()
+
     assert_no_left_over_files(tmp_path)
 
 
 def test_run_success_failure_mix(patching_storage, patching_rabbit, tmp_path):
     # Given
-    mock_processor = Mock(spec=Processor)
 
     def no_invalid(value, **_):
         if value == 'invalid':
             raise Invalid("Value cannot be 'invalid'")
 
-    mock_processor.schema = {'header': [no_invalid]}
-    mock_processor.build_event_messages.return_value = []
+    test_message = {"test_message": "blah"}
+
+    mock_processor = setup_mock_processor({'header': [no_invalid]}, test_message)
     bulk_processor = BulkProcessor(mock_processor)
     bulk_processor.working_dir = tmp_path
     mock_blob = Mock()
@@ -94,7 +95,21 @@ def test_run_success_failure_mix(patching_storage, patching_rabbit, tmp_path):
     assert call(str(tmp_path.joinpath('processed_mock_blob_name'))) in mock_upload_calls
     assert call(str(tmp_path.joinpath('failed_mock_blob_name'))) in mock_upload_calls
     assert call(str(tmp_path.joinpath('failure_reasons_mock_blob_name'))) in mock_upload_calls
+
+    patching_rabbit.return_value.__enter__.return_value.publish_message.assert_called_once_with(
+        message=json.dumps(test_message),
+        content_type='application/json', headers=None,
+        exchange=mock_processor.exchange, routing_key=mock_processor.routing_key)
     assert_no_left_over_files(tmp_path)
+
+
+def setup_mock_processor(schema, test_message):
+    mock_processor = Mock(spec=Processor)
+    mock_processor.schema = schema
+    mock_processor.build_event_messages.return_value = [test_message]
+    mock_processor.exchange = "events"
+    mock_processor.routing_key = 'test.mctest'
+    return mock_processor
 
 
 def mock_download_blob(_source_blob, destination_file, mock_data):
