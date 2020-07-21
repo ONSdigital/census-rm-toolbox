2c41169d73dd62442c8889a60507e86d5aaeaeb5
Apollo Akora <apollo.akora@ext.ons.gov.uk>
2020-07-21 09:25:20 +0100

    adding new reminder pack codes

--- reminder_batch_scheduler/constants.py
+++ reminder_batch_scheduler/constants.py
@@ -8,6 +8,7 @@ ACTION_TYPES_FOR_WAVE = {
     1: ['P_RL_1RL1_1', 'P_RL_1RL2B_1'],
     2: ['P_RL_2RL1', 'P_RL_2RL2B'],
     3: ['P_QU_H1', 'P_QU_H2'],
+    4: ['P_RL_1RL1B', 'P_RL_1RL2BB'],
 }
 
 WAVE_CLASSIFIERS = {
@@ -16,6 +17,8 @@ WAVE_CLASSIFIERS = {
     2: "treatment_code IN ('HH_LP1E', 'HH_LP1W', 'HH_LP2E', 'HH_LP2W') AND survey_launched = 'f'",
 
     3: "treatment_code IN ('HH_LP1E', 'HH_LP1W')",
+
+    4: "treatment_code IN ('HH_QP3E', 'HH_QP3W') AND survey_launched = 'f'",
 }
 
 ACTION_TYPE_CLASSIFIERS = {
@@ -29,5 +32,9 @@ ACTION_TYPE_CLASSIFIERS = {
 
     'P_QU_H1': "treatment_code IN ('HH_LP1E')",
 
-    'P_QU_H2': "treatment_code IN ('HH_LP1W')"
+    'P_QU_H2': "treatment_code IN ('HH_LP1W')",
+
+    'P_RL_1RL1B': "treatment_code IN ('HH_QP3E', 'HH_QP3W') AND survey_launched = 'f'",
+
+    'P_RL_1RL2BB': "treatment_code IN ('HH_QP3E', 'HH_QP3W') AND survey_launched = 'f'"
 }
