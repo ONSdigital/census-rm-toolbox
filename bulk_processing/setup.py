8c2647aea75c31e0b9c2e2634bd565409aea7754
AdamHawtin <adam.hawtin@ons.gov.uk>
2020-07-24 07:32:03 +0100

    Add submodule setup.py

---
+++ bulk_processing/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name="bulk_processing",
+    version="4.1.0a",
+    description="Process bulk events from files into RM event messages",
+    url="https://github.com/ONSdigital/census-rm-toolbox/tree/master/bulk_processing",
+    packages=setuptools.find_packages(),
+)
