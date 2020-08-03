58a1e24d739cca5f382a711ccbcd300d55e31f10
LeoKHoward <leo-howard@hotmail.co.uk>
2020-08-03 09:54:41 +0100

    Moved location and added error messages

--- bulk_processing/find_invalid_address_case_id_by_uprn.py
+++ bulk_processing/find_invalid_address_case_id_by_uprn.py
@@ -9,31 +9,32 @@ from requests import HTTPError
 from config import Config
 
 
-def read_invalid_address_uprn_file(file_to_process, project_id):
+def generate_bulk_invalid_address_file(file_to_process, project_id):
     case_id_list = []
     with open(file_to_process, encoding="utf-8") as open_file_to_process:
         file_reader = csv.DictReader(open_file_to_process, delimiter=',')
-        print(file_reader)
 
         for line_number, row in enumerate(file_reader, 1):
-            case_id_list.extend(get_case_id_from_case_api(row['UPRN']))
+            case_id_list.extend(get_case_id_from_case_api(row['UPRN'], line_number))
 
     address_delta_file = write_invalid_addresses_case_id_file(case_id_list, Path(file_to_process))
     upload_file_to_bucket(address_delta_file, project_id)
 
 
-def get_case_id_from_case_api(uprn):
+def get_case_id_from_case_api(uprn, line_number):
     response = requests.get(f'http://{Config.CASEAPI_HOST}:{Config.CASEAPI_PORT}/cases/uprn/{uprn}')
     try:
         response.raise_for_status()
-    except HTTPError:
-        print('Error: invalid or non-existent UPRN')
+    except HTTPError as e:
+        if response.status_code == 404:
+            print(f'Error 404: Cannot find the UPRN {uprn} on line {line_number}, Error {e}')
+        elif response.status_code != 404:
+            print(f'Network or Internal Server Error')
         return
 
     result = response.json()
 
-    case_id_list = [case['id'] for case in result]
-    return case_id_list
+    return [case['id'] for case in result]
 
 
 def write_invalid_addresses_case_id_file(case_id_list, file_to_process):
@@ -49,7 +50,7 @@ def write_invalid_addresses_case_id_file(case_id_list, file_to_process):
 
 def upload_file_to_bucket(file_path: Path, project_id):
     client = storage.Client(project=project_id)
-    bucket = client.get_bucket(f'{client.project}-bulk-invalid-address')
+    bucket = client.get_bucket(Config.BULK_INVALID_ADDRESS_BUCKET_NAME)
     print(f'Copying files to GCS bucket {bucket.name}')
     bucket.blob(f'{file_path.name}').upload_from_filename(filename=str(file_path))
     print(f'All files successfully written to {bucket.name}')
@@ -66,7 +67,7 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    read_invalid_address_uprn_file(args.file_to_process, args.project_id)
+    generate_bulk_invalid_address_file(args.file_to_process, args.project_id)
 
 
 if __name__ == '__main__':
