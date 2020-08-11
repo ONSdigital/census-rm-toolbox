06263a021773e3c83bf8b13f4dbcb3ffe58b687d
LeoKHoward <leo-howard@hotmail.co.uk>
2020-08-11 09:30:31 +0100

    Tests for code

--- toolbox/bulk_processing/find_invalid_address_case_id_by_uprn.py
+++ toolbox/bulk_processing/find_invalid_address_case_id_by_uprn.py
@@ -11,7 +11,7 @@ from toolbox.config import Config
 
 def generate_bulk_invalid_address_file(file_to_process):
     address_delta_file = Path(f'invalid_addresses_{file_to_process.stem}.csv')
-    error_count = ()
+    error = False
 
     with open(file_to_process, encoding="utf-8") as open_file_to_process:
         file_reader = csv.DictReader(open_file_to_process, delimiter=',')
@@ -24,15 +24,15 @@ def generate_bulk_invalid_address_file(file_to_process):
                 case_id_list = get_case_id_from_case_api(row['UPRN'], line_number)
                 if case_id_list:
                     for case_id in case_id_list:
-                        writer.writerow([f'{case_id}', 'ADDRESS_DELTA'])
+                        writer.writerow([case_id, 'ADDRESS_DELTA'])
                 else:
-                    error_count = True
+                    error = True
 
-            if error_count:
+            if error:
                 address_delta_file.unlink()
-                exit(1)
+                return None
 
-    # upload_file_to_bucket(address_delta_file)
+    return address_delta_file
 
 
 def get_case_id_from_case_api(uprn, line_number):
@@ -42,10 +42,9 @@ def get_case_id_from_case_api(uprn, line_number):
     except HTTPError as e:
         if response.status_code == 404:
             print(f'Error 404: Cannot find the UPRN {uprn} on line {line_number}, Error {e}')
-            return None
         else:
             print(f'Network or Internal Server Error on line {line_number}, Error {e}')
-            return None
+        return None
 
     result = response.json()
 
@@ -56,7 +55,7 @@ def upload_file_to_bucket(file_path: Path):
     client = storage.Client(project=Config.BULK_INVALID_ADDRESS_PROJECT_ID)
     bucket = client.get_bucket(Config.BULK_INVALID_ADDRESS_BUCKET_NAME)
     print(f'Copying files to GCS bucket {bucket.name}')
-    bucket.blob(f'{file_path.name}').upload_from_filename(filename=str(file_path))
+    bucket.blob(file_path.name).upload_from_filename(filename=str(file_path))
     print(f'All files successfully written to {bucket.name}')
 
     file_path.unlink()
@@ -70,7 +69,11 @@ def parse_arguments():
 
 def main():
     args = parse_arguments()
-    generate_bulk_invalid_address_file(Path(args.file_to_process))
+    address_delta_file = generate_bulk_invalid_address_file(Path(args.file_to_process))
+
+    if not address_delta_file:
+        exit(1)
+    upload_file_to_bucket(address_delta_file)
 
 
 if __name__ == '__main__':
