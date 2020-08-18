606fd9123beb15ab176bda016e56d7ef669af965
Nick Grant <nick.grant@ext.ons.gov.uk>
2020-08-18 08:24:16 +0100

    Fix text

--- splashscreen_prod.sh
+++ splashscreen_prod.sh
@@ -17,7 +17,7 @@ echo "            ░         ░ ░     ░       ░     ░ ░             
 echo "                            ░               ░"
 tput sgr0
 echo
-echo Give me commands I and I will do thy bidding. With great power comes great responsibility.
+echo Give me commands and I will do thy bidding. With great power comes great responsibility.
 echo
 echo Commands: helpme, msgwizard, findbad, baddetails, queuetool [-h], vi, curl, dosql [username], sftp, dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage, peekmessage, skipmessage, viewskipped, resetexceptionmanager, qidcheck [qid], qidlink, fulfilment, weekendfulfilment
 echo
