2737f081a56b7287cfd9f586f7ccb838874a0e0f
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2020-08-18 08:32:34 +0100

    Make production more visually obvious (#61)

--- splashscreen.sh
+++ splashscreen.sh
@@ -11,7 +11,7 @@ echo "██╔══██╗██║╚██╔╝██║       ██║ 
 echo "██║  ██║██║ ╚═╝ ██║       ██║   ╚██████╔╝╚██████╔╝███████╗███████║"
 echo "╚═╝  ╚═╝╚═╝     ╚═╝       ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝╚══════╝"
 echo
-echo Give me commands I and I will do thy bidding. With great power comes great responsibility.
+echo Give me commands and I will do thy bidding. With great power comes great responsibility.
 echo
 echo Commands: helpme, msgwizard, findbad, baddetails, queuetool [-h], vi, curl, dosql [username], sftp, dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage, peekmessage, skipmessage, viewskipped, resetexceptionmanager, qidcheck [qid], qidlink, fulfilment, weekendfulfilment
 echo
---
+++ splashscreen_prod.sh
@@ -0,0 +1,23 @@
+setterm -reset
+sleep 0.2s
+
+echo
+echo Welcome to even more bettererer...
+echo
+setterm -term linux -fore yellow
+echo " ██▓███   ██▀███   ▒█████  ▓█████▄  █    ██  ▄████▄  ▄▄▄█████▓ ██▓ ▒█████   ███▄    █"
+echo "▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒▒██▀ ██▌ ██  ▓██▒▒██▀ ▀█  ▓  ██▒ ▓▒▓██▒▒██▒  ██▒ ██ ▀█   █"
+echo "▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒░██   █▌▓██  ▒██░▒▓█    ▄ ▒ ▓██░ ▒░▒██▒▒██░  ██▒▓██  ▀█ ██▒"
+echo "▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░░▓█▄   ▌▓▓█  ░██░▒▓▓▄ ▄██▒░ ▓██▓ ░ ░██░▒██   ██░▓██▒  ▐▌██▒"
+echo "▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░░▒████▓ ▒▒█████▓ ▒ ▓███▀ ░  ▒██▒ ░ ░██░░ ████▓▒░▒██░   ▓██░"
+echo "▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░  ▒▒▓  ▒ ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░  ▒ ░░   ░▓  ░ ▒░▒░▒░ ░ ▒░   ▒ ▒"
+echo "░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░  ░ ▒  ▒ ░░▒░ ░ ░   ░  ▒       ░     ▒ ░  ░ ▒ ▒░ ░ ░░   ░ ▒░"
+echo "░░         ░░   ░ ░ ░ ░ ▒   ░ ░  ░  ░░░ ░ ░ ░          ░       ▒ ░░ ░ ░ ▒     ░   ░ ░"
+echo "            ░         ░ ░     ░       ░     ░ ░                ░      ░ ░           ░"
+echo "                            ░               ░"
+tput sgr0
+echo
+echo Give me commands and I will do thy bidding. With great power comes great responsibility.
+echo
+echo Commands: helpme, msgwizard, findbad, baddetails, queuetool [-h], vi, curl, dosql [username], sftp, dumpfilestoqueue, dumpqueuetofiles, listqueues, makemessage, peekmessage, skipmessage, viewskipped, resetexceptionmanager, qidcheck [qid], qidlink, fulfilment, weekendfulfilment
+echo
