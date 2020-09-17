9e897079a5c96e7b7b2cbc78bf9a5e4da0346005
Nick Grant <41681172+NickGrantONS@users.noreply.github.com>
2020-09-17 12:16:38 +0100

    Hide one off exceptions in bad message wizard (#67)

--- toolbox/message_tools/bad_message_wizard.py
+++ toolbox/message_tools/bad_message_wizard.py
@@ -188,7 +188,7 @@ def show_bad_message_list():
 
 
 def get_message_summaries():
-    response = requests.get(f'{Config.EXCEPTIONMANAGER_URL}/badmessages/summary')
+    response = requests.get(f'{Config.EXCEPTIONMANAGER_URL}/badmessages/summary?minimumSeenCount=2')
     response.raise_for_status()
     return response.json()
 
