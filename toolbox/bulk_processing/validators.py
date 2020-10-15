d51c0774e3c9e47bdb9332951a629aacf32b9a91
Josh-McColl <joshua.mccoll@ext.ons.gov.uk>
2020-10-15 14:19:15 +0100

    Fix bulknewaddresses error on region empty

--- toolbox/bulk_processing/validators.py
+++ toolbox/bulk_processing/validators.py
@@ -141,10 +141,12 @@ def no_pipe_character():
 
 def region_matches_treatment_code():
     def validate(region, **kwargs):
-        if region[0] != kwargs['row']['TREATMENT_CODE'][-1]:
-            raise Invalid(
-                f'Region "{region}" does not match region in treatment code "{kwargs["row"]["TREATMENT_CODE"]}"')
-
+        try:
+            if region[0] != kwargs['row']['TREATMENT_CODE'][-1]:
+                raise Invalid(
+                    f'Region "{region}" does not match region in treatment code "{kwargs["row"]["TREATMENT_CODE"]}"')
+        except IndexError:
+            pass
     return validate
 
 
