3721b279c48672b80c8298638e429f6fe519b11d
liamtoozer <liam.toozer@gmail.com>
2020-11-10 08:53:09 +0000

    Tests

---
+++ toolbox/reminder_scheduler/reminder_lsoa.py
@@ -0,0 +1,126 @@
+import argparse
+import csv
+import uuid
+from datetime import datetime
+from pathlib import Path
+
+import rfc3339
+from termcolor import colored
+
+from toolbox.config import Config
+from toolbox.reminder_scheduler import constants
+from toolbox.utilities import db_helper
+
+
+def main(lsoa_file_path: Path, reminder_action_rule: str, action_plan_id: uuid.UUID, insert_rule: bool = False,
+         trigger_date_time: datetime = None):
+    lsoas = get_lsoas_from_file(lsoa_file_path)
+    action_rule_classifiers = build_action_rule_classifiers(lsoas)
+
+    print()
+    print(f'Reminder Action rule: {reminder_action_rule}')
+    print('Classifiers clause:')
+    print(action_rule_classifiers)
+
+    if insert_rule:
+        action_rule = generate_action_rule(reminder_action_rule, action_rule_classifiers, action_plan_id,
+                                           trigger_date_time)
+        print()
+        print('Generated action rule:')
+        print(action_rule)
+        print('')
+        print('About to insert action rule for:')
+        print('Action rule:', colored(reminder_action_rule, "red"))
+        print('Trigger date time:', colored(trigger_date_time.isoformat(), 'red'))
+        if not confirm_insert_rule():
+            print(colored('ABORTING', 'red'))
+            return
+        insert_action_rule(action_rule)
+        print()
+        print(colored("Action rule inserted", 'green'))
+
+
+def get_lsoas_from_file(lsoa_file_path):
+    with open(lsoa_file_path) as lsoa_file:
+        lsoa_file_reader = csv.reader(lsoa_file, delimiter=',')
+        return [lsoa[0] for lsoa in lsoa_file_reader]
+
+
+def build_action_rule_classifiers(lsoas):
+    lsoa_classifier_clause = "', '".join(lsoas)
+    return f"case_type = 'HH' " \
+           f"AND lsoa IN ('{lsoa_classifier_clause}')"
+
+
+def confirm_insert_rule():
+    confirmation = input(
+        colored('WARNING: You are about to write the the action rule to the database, '
+                'resulting in materials being sent for print. \nContinue? [Y/n] ', color='red'))
+    if confirmation != 'Y' and confirmation.lower() != 'yes':
+        return False
+    return True
+
+
+def generate_action_rule(action_type, action_rule_classifiers, action_plan_id, trigger_date_time: datetime):
+    action_rule = (
+        "INSERT INTO actionv2.action_rule "
+        "(id, action_type, classifiers_clause, trigger_date_time, action_plan_id, has_triggered) "
+        "VALUES (%s, %s, %s, %s, %s, %s);",
+        (str(uuid.uuid4()), action_type, action_rule_classifiers, trigger_date_time, str(action_plan_id), False)
+    )
+    return action_rule
+
+
+def insert_action_rule(action_rule):
+    with db_helper.open_write_cursor(Config.DB_HOST_ACTION, Config.DB_ACTION_CERTIFICATES) as db_cursor:
+        print("Inserting action rule", action_rule)
+        db_helper.execute_sql_query_with_write(db_cursor, action_rule[0], action_rule[1])
+
+
+def parse_trigger_date_time(trigger_date_time):
+    if trigger_date_time:
+        return rfc3339.parse_datetime(args.trigger_date_time)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(
+        description="Insert action rule from a provided list of LSOAs")
+    parser.add_argument('lsoa_file_path',
+                        help='Path to the LSOAs file',
+                        type=str)
+    parser.add_argument('-r', '--reminder-action-rule',
+                        help='The response driven reminder Action Rule',
+                        required=True,
+                        type=str,
+                        choices=constants.ACTION_RULES_FOR_RESPONSE_DRIVEN_REMINDER)
+    parser.add_argument('-a', '--action-plan-id',
+                        help='Action plan UUID',
+                        required=True,
+                        type=uuid.UUID)
+    parser.add_argument('--insert-rule',
+                        help='!!! Insert the generated rule into the action database !!!',
+                        required=False,
+                        action='store_true')
+    parser.add_argument('--trigger-date-time',
+                        help='Action rule trigger date time in RFC3339 format (required when inserting action rule)',
+                        required=False,
+                        default=None)
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    if args.insert_rule and not args.trigger_date_time:
+        print("Cannot insert action rule without '--trigger-date-time' try '-h' for help")
+        exit(1)
+    try:
+        parsed_trigger_date_time = parse_trigger_date_time(args.trigger_date_time)
+    except Exception:
+        print('Trigger date time must be RFC3339 format')
+        raise
+
+    main(args.lsoa_file_path,
+         args.reminder_action_rule,
+         args.action_plan_id,
+         args.insert_rule,
+         parsed_trigger_date_time)
