230ff51d8c1c7f6ef0742cfff4060a58fd896a08
liamtoozer <liam.toozer@gmail.com>
2020-12-31 09:08:55 +0000

    Running as non-root

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,22 @@ FROM python:3.7-slim
 RUN pip install pipenv
 
 COPY .bashrc_extras /tmp
+#COPY toolboxjob /tmp
 
 RUN apt-get update && apt-get -yq install curl && apt-get -yq install jq && apt-get -yq install vim-tiny && \
     apt-get -yq install unzip && apt-get -yq install postgresql-client || true && \
     apt-get -yq install openssh-client || true && apt-get -yq install procps || true && \
-    apt-get -yq clean && groupadd --gid 1000 toolbox && \
+    apt-get -yq clean && apt-get -yq install cron && groupadd --gid 1000 toolbox && \
     useradd --create-home --system --uid 1000 --gid toolbox toolbox && \
     cat /tmp/.bashrc_extras >> /home/toolbox/.bashrc && rm /tmp/.bashrc_extras
+
+RUN touch /var/log/cron.log
+COPY toolboxjob /etc/cron.d/toolboxjob
+#RUN chmod 0644 /etc/cron.d/toolboxjob
+#RUN --chown=toolbox /etc/cron.d/toolboxjob
+#CMD cron
+#RUN cron /etc/cron.d/cjob
+
 WORKDIR /home/toolbox
 
 ENV RABBITMQ_SERVICE_HOST rabbitmq
@@ -21,7 +30,7 @@ ENV RABBITMQ_PASSWORD guest
 
 COPY Pipfile* /home/toolbox/
 RUN pipenv install --system --deploy
-USER toolbox
+#USER toolbox
 
 RUN mkdir /home/toolbox/.postgresql &&  mkdir /home/toolbox/.postgresql-rw &&  mkdir /home/toolbox/.postgresql-action
 
---
+++ toolbox/audit_backup/put_audit_files_to_bucket.py
@@ -0,0 +1,34 @@
+import argparse
+from pathlib import Path
+
+from google.cloud import storage
+
+
+def put_audit_files_to_bucket(audit_files_folder, project_name, bucket_name):
+    audit_folders = [folder for folder in audit_files_folder.iterdir() if folder.is_dir()]
+    for folder in audit_folders:
+        log_files = list(folder.glob('*.log'))
+        if log_files:
+            for log_file in log_files:
+                client = storage.Client(project=project_name)
+                bucket = client.get_bucket(bucket_name)
+                print(f'Copying {log_file} to GCS bucket {bucket.name}')
+                bucket.blob(f'{log_file.parent.name}/{log_file.name}').upload_from_filename(filename=log_file)
+                print(f'File successfully written {log_file} to {bucket.name}')
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Upload a file to a bucket')
+    parser.add_argument('audit_files_folder', help='Location of audit files', type=Path)
+    parser.add_argument('project_name', help='project name', type=str)
+    parser.add_argument('bucket_name', help='bucket name', type=str)
+    return parser.parse_args()
+
+
+def main():
+    args = parse_arguments()
+    put_audit_files_to_bucket(args.audit_files_folder, args.project_name, args.bucket_name)
+
+
+if __name__ == "__main__":
+    main()
