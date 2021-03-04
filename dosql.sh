3e6f56917bd0dedc1d85c4e1755b153eff85ba94
Nick Grant <nick.grant@ext.ons.gov.uk>
2021-03-04 15:18:21 +0000

    Enable autocommit for read-only dosql sessions

--- dosql.sh
+++ dosql.sh
@@ -3,4 +3,4 @@ if [[ -z "$1" ]]; then
 else
   username=$1
 fi
-psql "sslmode=require hostaddr=$DB_HOST user=$username dbname=$DB_NAME" -e -L ~/.audit/$CURRENT_USER/sql_${username}_$(date --iso-8601=ns).log
+psql "sslmode=require hostaddr=$DB_HOST user=$username dbname=$DB_NAME" --no-psqlrc -e -L ~/.audit/$CURRENT_USER/sql_${username}_$(date --iso-8601=ns).log
