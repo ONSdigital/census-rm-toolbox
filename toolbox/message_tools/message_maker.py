82b2139a893c249dd78385004f19c96534072d92
Adam <35268982+AdamHawtin@users.noreply.github.com>
2021-03-17 10:59:40 +0000

    Add message maker for RM UAC CREATED (#103)

--- toolbox/message_tools/message_maker.py
+++ toolbox/message_tools/message_maker.py
@@ -5,8 +5,10 @@ from datetime import datetime
 import requests
 from requests import HTTPError
 
-from toolbox.utilities.rabbit_context import RabbitContext
 from toolbox.config import Config
+from toolbox.qid_checksum_validator import validate
+from toolbox.utilities.db_helper import execute_parametrized_sql_query
+from toolbox.utilities.rabbit_context import RabbitContext
 
 
 def create_questionnaire_linked_message():
@@ -122,16 +124,98 @@ def create_refusal_message():
         print('Successfully published')
 
 
+def check_uac_qid_pair_exist(uac, qid):
+    result = execute_parametrized_sql_query("SELECT qid FROM uacqid.uac_qid WHERE uac = %s", (uac,))
+    if not result:
+        raise ValueError('ERROR: UAC not found in uac qid database')
+
+    found_qid = result[0][0]
+    if found_qid != qid:
+        raise ValueError(f'ERROR: UAC exists but is linked to a different QID: {found_qid}')
+
+
+def create_rm_uac_created_message():
+    print('!!! WARNING !!!')
+    print('You must be absolutely sure this UAC/QID pair is genuine, it MUST exist in the uacqid service database '
+          'but NOT already exist in our UAC QID link table')
+    speed_bump_response = input('Are you sure you wish to proceed? (y/n): ')
+    if speed_bump_response.lower() not in {'yes', 'y'}:
+        return
+    print('Ok, generating an RM UAC CREATED message.')
+    print('Whats the QID?')
+    qid = input()
+
+    if not validate(qid, int(Config.QID_MODULUS), int(Config.QID_FACTOR)):
+        raise ValueError('ERROR: Bad QID, checksum invalid')
+
+    qid_check_response = requests.get(f'http://{Config.CASEAPI_HOST}:{Config.CASEAPI_PORT}/qids/{qid}')
+    if qid_check_response.status_code != 404:
+        raise ValueError('ERROR: This QID already exists in the uac qid link table')
+
+    print('What is the UAC?')
+    uac = input()
+    if len(uac) != 16:
+        raise ValueError('Bad UAC, wrong length. Message not sent.')
+
+    if not uac.isalnum():
+        raise ValueError('Bad UAC, bad characters. Message not sent.')
+
+    check_uac_qid_pair_exist(uac, qid)
+
+    print('Whats the CaseID?')
+    case_id = input()
+
+    case_check_response = requests.get(f'http://{Config.CASEAPI_HOST}:{Config.CASEAPI_PORT}/cases/{case_id}')
+
+    try:
+        case_check_response.raise_for_status()
+    except HTTPError:
+        raise ValueError('Error: invalid or non-existent Case ID. Message not sent.')
+
+    message = {
+        "event": {
+            "type": "RM_UAC_CREATED",
+            "source": "RM",
+            "channel": "RM",
+            "dateTime": datetime.utcnow().isoformat() + 'Z',
+            "transactionId": str(uuid.uuid4())
+        },
+        "payload": {
+            "uacQidCreated": {
+                "caseId": case_id,
+                "qid": qid,
+                "uac": uac
+            }
+        }
+    }
+
+    print('Here is the generated message:')
+    print(json.dumps(message, sort_keys=True, indent=4))
+    print('Do you want to publish it?')
+    do_publish = input()
+
+    if do_publish != 'yes':
+        print('Not publishing')
+        return
+
+    with RabbitContext(queue_name='case.uac-qid-created') as rabbit:
+        rabbit.publish_message(json.dumps(message), 'application/json', None, routing_key='case.uac-qid-created')
+        print('Successfully published')
+
+
 def main():
     print('What type of message do you want?')
     print('1. Refusal')
     print('2. Questionnaire Linked')
+    print('3. RM UAC CREATED')
     message_type = input()
 
     if message_type == '1':
         create_refusal_message()
     elif message_type == '2':
         create_questionnaire_linked_message()
+    elif message_type == '3':
+        create_rm_uac_created_message()
     else:
         print('Error: you must choose a valid option')
 
